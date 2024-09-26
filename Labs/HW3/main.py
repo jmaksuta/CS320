2e1f49eb53eeb141a4df360965a987564cd9ed89
jmaksuta <jmaksuta@colostate.edu>
2024-09-25 22:11:03 -0600

    Changed the main.py to the version with no class. It is more efficient in testing and it passes all the tests.

--- Labs/HW3/main.py
+++ Labs/HW3/main.py
@@ -7,94 +7,98 @@ Dr. Partridge
 """
 
 
-class Heap:
-    ''' Class representing the heap in a heap sort. '''
-    def __init__(self) -> None:
-        self.list = []
+def _swap(the_heap, up_elem_index, down_elem_index):
+    """ swaps the elements in the list at indices up_elem_index
+    and down_elem_index. """
+    temp = the_heap[up_elem_index]
+    the_heap[up_elem_index] = the_heap[down_elem_index]
+    the_heap[down_elem_index] = temp
+    return
 
-    def _swap(self, up_elem_index, down_elem_index):
-        """ swaps the elements in the list at indices up_elem_index
-        and down_elem_index. """
-        temp = self.list[up_elem_index]
-        self.list[up_elem_index] = self.list[down_elem_index]
-        self.list[down_elem_index] = temp
-        return
 
-    def _greater_than(self, left_operand, right_operand):
-        """ Returns True if left_operand is greater than right_operand. """
-        return left_operand > right_operand
+def _greater_than(left_operand, right_operand):
+    """ Returns True if left_operand is greater than right_operand. """
+    return left_operand > right_operand
 
-    def insert(self, element=None) -> None:
-        """ Inserts an element into the heap and heapifys. """
-        self.list.append(element)
-        last_index = len(self.list) - 1
-        current_index = last_index
-        while current_index > 0 and self._greater_than(self.list[int((current_index - 1) / 2)],
-                                                       self.list[current_index]):
-            parent_index = int((current_index - 1) / 2)
-            child_index = current_index
-            self._swap(parent_index, child_index)
-            current_index = parent_index
-        return
-    
-    def _index_of_smaller(self, index_a, index_b):
-        """ Compares element at index_a with element at index_b and
-        returns the index of the smaller of the two."""
-        result = -1
-        if self.list[index_a] <= self.list[index_b]:
-            result = index_a
-        else:
-            result = index_b
-        return result
 
-    def _to_index(self, nth_value):
-        """ Returns the index value an the nth value.
-        i.e. the nth element has an index n - 1. """
-        return nth_value - 1
-    
-    def _nth_elem(self, n):
-        """ Returns the nth element. """
-        return self.list[self._to_index(n)]
+def insert(the_heap, element=None) -> None:
+    """ Inserts an element into the heap and heapifys. """
+    the_heap.append(element)
+    last_index = len(the_heap) - 1
+    current_index = last_index
+    while current_index > 0 and _greater_than(the_heap[int((current_index - 1) / 2)],
+                                              the_heap[current_index]):
+        parent_index = int((current_index - 1) / 2)
+        child_index = current_index
+        _swap(the_heap, parent_index, child_index)
+        current_index = parent_index
+    return
 
-    def remove_minimum_element(self):
-        """ Removes the minimum element from the heap, the first element,
-        and heapifys"""
-        temp = self.list[0]
-        length_of_list = len(self.list)
-        self.list[0] = self._nth_elem(length_of_list)
-        length_of_list = length_of_list - 1
-        self.list = self.list[:length_of_list]
-        current_index = 0
-        while current_index < length_of_list:
-            index_a = (2 * current_index) + 1
-            index_b = (2 * current_index) + 2
-            if index_b < length_of_list:
-                current_less_than_child_a = self.list[current_index] <= self.list[index_a]
-                current_less_than_child_b = self.list[current_index] <= self.list[index_b]
-                if current_less_than_child_a and current_less_than_child_b:
-                    return temp
-                else:
-                    smaller_index = self._index_of_smaller(index_a, index_b)
-                    self._swap(current_index, smaller_index)
-                    current_index = smaller_index
-            else:
-                if index_a < length_of_list:
-                    if self.list[current_index] > self.list[index_a]:
-                        self._swap(current_index, index_a)
+
+def _index_of_smaller(the_heap, index_a, index_b):
+    """ Compares element at index_a with element at index_b and
+    returns the index of the smaller of the two."""
+    result = -1
+    if the_heap[index_a] <= the_heap[index_b]:
+        result = index_a
+    else:
+        result = index_b
+    return result
+
+
+def _to_index(nth_value):
+    """ Returns the index value an the nth value.
+    i.e. the nth element has an index n - 1. """
+    return nth_value - 1
+
+
+def _nth_elem(the_heap, n):
+    """ Returns the nth element. """
+    return the_heap[_to_index(n)]
+
+
+def remove_minimum_element(the_heap):
+    """ Removes the minimum element from the heap, the first element,
+    and heapifys"""
+    temp = the_heap[0]
+    length_of_list = len(the_heap)
+    the_heap[0] = _nth_elem(the_heap, length_of_list)
+    length_of_list = length_of_list - 1
+    # the_heap = the_heap[:length_of_list]
+    del the_heap[length_of_list]
+    current_index = 0
+    while current_index < length_of_list:
+        index_a = (2 * current_index) + 1
+        index_b = (2 * current_index) + 2
+        if index_b < length_of_list:
+            current_less_than_child_a = the_heap[current_index] <= the_heap[index_a]
+            current_less_than_child_b = the_heap[current_index] <= the_heap[index_b]
+            if current_less_than_child_a and current_less_than_child_b:
                 return temp
-        return temp           
+            else:
+                smaller_index = _index_of_smaller(the_heap, index_a, index_b)
+                _swap(the_heap, current_index, smaller_index)
+                current_index = smaller_index
+        else:
+            if index_a < length_of_list:
+                if the_heap[current_index] > the_heap[index_a]:
+                    _swap(the_heap, current_index, index_a)
+            return temp
+    return temp           
 
 
 def internal_heapsort(hlist):
     """ Performs the heapsort on the list. """
     result = []
-    the_heap = Heap()
+    the_heap = []
     for index in range(0, len(hlist)):
         input_value = int(hlist[index])
-        the_heap.insert(input_value)
+        insert(the_heap, input_value)
 
-    for index in range(len(the_heap.list)):
-        result.append(the_heap.remove_minimum_element())
+    # length = len(the_heap)
+    # for index in range(len(the_heap)):
+    while len(the_heap) > 0: 
+        result.append(remove_minimum_element(the_heap))
 
     return result
 
