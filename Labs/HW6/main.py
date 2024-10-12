898422968ac9d7d9056fa023c317de1381f3973b
jmaksuta <jmaksuta@colostate.edu>
2024-10-12 00:28:25 -0600

    Added the edgegraph import statement.

--- Labs/HW6/main.py
+++ Labs/HW6/main.py
@@ -6,6 +6,7 @@ CS320-801 Fall 2024
 Dr. Partridge
 """
 
+from edgegraph import VertexEL, EdgeEL, GraphEL, parse_graph_file
 
 # 64.2 Depth-First Traversal
 # In this lab, you are tasked with writing a Depth-First traversal (see textbook section 14.3) using the course-provided edgegraph library. Download the edgegraph.py file below. (This version of edgegraph.py may be improved from the version you downloaded from canvas. Make sure you use the one provided with this assignment).
