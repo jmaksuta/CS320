34872cf95c0b796739c6a1188d0cc275bdac9e96
jmaksuta <jmaksuta@colostate.edu>
2024-10-12 00:24:34 -0600

    Created the main.py module for my assignment. Moved the comment block with the assignment description from edgegraph.py to main.py. We are importing from edgegraph.py, not modifying it.

--- Labs/HW6/edgegraph.py
+++ Labs/HW6/edgegraph.py
@@ -172,16 +172,3 @@ def parse_graph_file(file_path):
             graph.add_edge(edge)
 
     return graph
-
-# 64.2 Depth-First Traversal
-# In this lab, you are tasked with writing a Depth-First traversal (see textbook section 14.3) using the course-provided edgegraph library. Download the edgegraph.py file below. (This version of edgegraph.py may be improved from the version you downloaded from canvas. Make sure you use the one provided with this assignment).
-
-# Specifically, you will write a routine dfs(graph, start) which accepts a graph (GraphEL) and a start vertex (VertexEL) and returns a tuple of vertices in a valid DFS order.
-
-# Some details:
-# You may assume that graph is not a forest.
-# If graph is None, return an empty tuple (())
-# If start is None, return an empty tuple (())
-# If start is not in graph, return an empty tuple(())
-# Performance
-# Your implementation should run in time O(n+m), where n is the number of vertices and m is the number of edges.
