3afab665003ac78b164f7c99f99b7275c3c832a8
jmaksuta <jmaksuta@colostate.edu>
2024-10-13 14:11:09 -0600

    Applied pycodestyle changes for the PEP8 compliance.

--- Labs/HW6/main.py
+++ Labs/HW6/main.py
@@ -8,22 +8,12 @@ Dr. Partridge
 
 from edgegraph import VertexEL, EdgeEL, GraphEL, parse_graph_file
 
-# 64.2 Depth-First Traversal
-# In this lab, you are tasked with writing a Depth-First traversal (see textbook section 14.3) using the course-provided edgegraph library. Download the edgegraph.py file below. (This version of edgegraph.py may be improved from the version you downloaded from canvas. Make sure you use the one provided with this assignment).
-
-# Specifically, you will write a routine dfs(graph, start) which accepts a graph (GraphEL) and a start vertex (VertexEL) and returns a tuple of vertices in a valid DFS order.
-
-# Some details:
-# You may assume that graph is not a forest.
-# If graph is None, return an empty tuple (())
-# If start is None, return an empty tuple (())
-# If start is not in graph, return an empty tuple(())
-# Performance
-# Your implementation should run in time O(n+m), where n is the number of vertices and m is the number of edges.
 EXPLORED = 1
 UNEXPLORED = 2
 
+
 def get_end_vertex(edge, start):
+    """ Returns the end vertex of edge that is opposite of start. """
     end_vertex = None
     ends = edge.ends()
     if ends[0] == start:
@@ -32,13 +22,14 @@ def get_end_vertex(edge, start):
         end_vertex = ends[0]
     return end_vertex
 
+
 def is_unexplored(paths, edge):
     return (paths.get(str(edge)) != EXPLORED)
 
 
 def _internal_dfs(graph, start, paths=None, depth=0):
     discover_path = []
-    if paths == None:
+    if paths is None:
         paths = dict()
     paths[str(start)] = EXPLORED
     discover_path.append(start)
@@ -59,15 +50,13 @@ def _internal_dfs(graph, start, paths=None, depth=0):
         result = (discover_path, paths)
     return result
 
+
 def validate(graph, start):
     """ Validates the input argments. """
-    # You may assume that graph is not a forest.
-    # If graph is None, return an empty tuple (())
-    # If start is None, return an empty tuple (())
-    # If start is not in graph, return an empty tuple(())
     assert graph is not None
     assert start is not None
     assert start in graph.vertices()
+    
     
 def dfs(graph, start):
     result = (())
