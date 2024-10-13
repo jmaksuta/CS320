50881f7a8601416c0d898e1084a7de96fcc05fdd
jmaksuta <jmaksuta@colostate.edu>
2024-10-13 13:52:13 -0600

    current progress.

--- Labs/HW6/main.py
+++ Labs/HW6/main.py
@@ -20,8 +20,38 @@ from edgegraph import VertexEL, EdgeEL, GraphEL, parse_graph_file
 # If start is not in graph, return an empty tuple(())
 # Performance
 # Your implementation should run in time O(n+m), where n is the number of vertices and m is the number of edges.
-def _internal_dfs(graph, start):
-    pass
+EXPLORED = 1
+UNEXPLORED = 2
+
+def is_unexplored(paths, edge):
+    return (paths.get(str(edge)) != EXPLORED)
+
+
+def _internal_dfs(graph, start, paths=None):
+    result = (())
+    discover_path = []
+    if paths == None:
+        paths = dict()
+    paths[str(start)] = EXPLORED
+    discover_path.append(start)
+    for edge in graph.incident(start):
+        print(edge)
+        if is_unexplored(paths, edge):
+            ends = edge.ends()
+            end_vertex = None
+            if ends[0] == start:
+                end_vertex = ends[1]
+            if ends[1] == start:
+                end_vertex = ends[0]
+            if is_unexplored(paths, end_vertex):
+                paths[str(edge)] = "Discovery"
+                res, route, paths = _internal_dfs(graph, end_vertex, paths)
+                print("DEBUG: ", res, str(route), str(paths))
+            else:
+                paths[str(edge)] = "Back"
+    print("DEBUG: paths=" + str(paths))
+    print("DEBUG: discover_path=" + str(discover_path))
+    return result, discover_path, paths
 
 def validate(graph, start):
     """ Validates the input argments. """
@@ -38,8 +68,10 @@ def dfs(graph, start):
     try:
         validate(graph, start)
         result = _internal_dfs(graph, start)
+        print(graph)
 
     except Exception as e:
         # result = None
+        print(e)
         pass
     return result
