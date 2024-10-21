f4dd545635da22c99d9025f284133c9d4427f214
jmaksuta <jmaksuta@colostate.edu>
2024-10-20 22:22:14 -0600

    Current progress. Stopping for the night. too tired.

--- Labs/HW7/main.py
+++ Labs/HW7/main.py
@@ -8,23 +8,73 @@ Dr. Partridge
 
 from edgegraph import *
 import math
+import sys
+
+def weight(edge):
+    return 1
+
+def relaxation(distances, edge):
+    u = edge.head
+    z = edge.tail
+    edge_weight = weight(edge)
+    if distances[str(u)] + edge_weight < distances[str(z)]:
+    # if D[u] + w((u, z)) < D[z] then
+        distances[str(z)] = distances[str(u)] + edge_weight
+        #D[z] = D[u] + w((u, z))
+    return distances
+
+def get_other_end(edge, vertex):
+    result = None
+    pair = edge.ends()
+    if pair[0] == vertex:
+        result = pair[1]
+    elif pair[1] == vertex:
+        result = pair[0]
+    return result
+
+
+def no_relaxation_possible(graph: GraphEL):
+    return True
+
+def package_result(distances: dict):
+    result = []
+    # return the label D[u] of each vertex u
+    for key, value in distances.items():
+        result.append(key)
+    return result
+
 
 def _bellman_ford(graph: GraphEL, start: VertexEL, end: VertexEL) -> list:
     """ A weighted directed graph with n vertices, and a vertex v of G. """
     # input
-    path = dict()
-    path[start] = 0
+    distances = dict()
+    paths = dict()
+    distances[str(start)] = 0
+    paths[str(start)] = [start]
     # D[v] = 0
-    # for each vertex u != v of G do
-        # D[u] = positive infinity
-    # for i = 1 to n-1 do
-        # for each (directed) edge (u, z) outgoing from u do
-            # Perform the relax ation operation on (u,z)
+    for vertex in graph.vertices():
+        if vertex is not start:
+            distances[str(vertex)] = sys.maxsize
+            paths[str(vertex)] = [vertex]
+    
+    for u in graph.vertices():
+        for dir_edge in graph.out_incident(u):
+            z = dir_edge.tail
+            # Perform the relaxation operation on (u,z)
+            distances = relaxation(dir_edge)
+            edge_weight = weight(dir_edge)
+            if distances[str(u)] + edge_weight < distances[str(z)]:
             # if D[u] + w((u, z)) < D[z] then
+                distances[str(z)] = distances[str(u)] + edge_weight
+                paths[str(u)] = paths[str(u)] + distances[str(z)]
                 # D[z] = D[u] + w((u, z))
+    if no_relaxation_possible(graph):
     # if there are no edges left with potential relaxation operations then
         # return the label D[u] of each vertex u
+        return package_result(distances)
+    else:
     # else
+        return None
         # return "Graph contatins a negative-weight cycle"
 
 
