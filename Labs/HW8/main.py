7fd49115e61af38e5ab888cbde3807109f3bfca6
jmaksuta <jmaksuta@colostate.edu>
2024-11-05 01:32:37 -0700

    It only failed the PEP8 style and one validation. So I applied PEP8 style and added the missing validation. Added documentation comments for methods.

--- Labs/HW8/main.py
+++ Labs/HW8/main.py
@@ -9,52 +9,49 @@ Dr. Partridge
 import copy
 from util import cost, valid_path, best_path
 
-def get_cost(path, distances):
-    result = 0
-    for index in range(0, len(path)):
-        a = path[index - 1]
-        b = path[index]
-        result += distances[(a,b)]
-    return result
+
+def replace_parents_with_valid_children(population, distances, index,
+                                        temp_last, temp_path, last, path):
+    """ Replaces parents with valid children that are better than parents. """
+    if valid_path(temp_last) and cost(temp_last, distances) < cost(last, distances):
+        population[index - 1] = temp_last
+
+    if valid_path(temp_path) and cost(temp_path, distances) < cost(path, distances):
+        population[index] = temp_path
+    return population
 
 
-def generation(population, distances, gen):
-    # best = None
+def generation(population, distances) -> tuple:
+    """ Returns the population and distances. """
     for index in range(1, len(population), 2):
         last = population[index - 1]
-        # if best == None:
-        #     best = last
         path = population[index]
-        # last_cost = get_cost(last, distances)
-        # path_cost = get_cost(path, distances)
-        # is_valid = valid_path(path)
-        # is_improved = (path_cost < last_cost)
-        # if is_valid and is_improved:
-        #     best = path
-        # else:
+
         temp_path = list(copy.deepcopy(path))
         temp_last = list(copy.deepcopy(last))
-        # step = 2 + (gen % 2)
-        step = 1
-        for place in range(0, len(path), step):            
+
+        for place in range(0, len(path), 1):            
             swap_a = temp_last[place]
             swap_b = temp_path[place]
             if swap_a != swap_b:
+                # we will swap the current value, from each parent,
+                # removing existing swap value first
                 i_last = temp_last.index(swap_b)
                 i_path = temp_path.index(swap_a)
                 temp_last = temp_last[:i_last] + temp_last[i_last + 1:]
                 temp_path = temp_path[:i_path] + temp_path[i_path + 1:]
                 temp_last = temp_last[:place] + [swap_b] + temp_last[place:]
                 temp_path = temp_path[:place] + [swap_a] + temp_path[place:]
-
         temp_last = tuple(temp_last)
         temp_path = tuple(temp_path)
         
-        if valid_path(temp_last) and cost(temp_last, distances) < cost(last, distances):
-            population[index - 1] = temp_last
+        population = replace_parents_with_valid_children(population, distances, index,
+                                                         temp_last, temp_path, last, path)
+        # if valid_path(temp_last) and cost(temp_last, distances) < cost(last, distances):
+        #     population[index - 1] = temp_last
 
-        if valid_path(temp_path) and cost(temp_path, distances) < cost(path, distances):
-            population[index] = temp_path
+        # if valid_path(temp_path) and cost(temp_path, distances) < cost(path, distances):
+        #     population[index] = temp_path
 
     return (population, distances)
 
@@ -62,7 +59,7 @@ def generation(population, distances, gen):
 def _ga_tsp(initial_population, distances, generations) -> tuple:
     population = copy.deepcopy(initial_population)
     for gen in range(0, generations):
-        population, distances = generation(population, distances, gen)
+        population, distances = generation(population, distances)
 
     result = best_path(initial_population, distances)
     best = best_path(population, distances)
@@ -79,7 +76,6 @@ def validate(initial_population, distances, generations):
     assert initial_population is not None
     assert distances is not None
     assert generations is not None
-     # if generations is not positive, ga_tsp should return None
     assert generations > 0
     return
 
